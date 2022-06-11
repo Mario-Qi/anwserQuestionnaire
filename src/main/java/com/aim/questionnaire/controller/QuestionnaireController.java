86fe8bff018327127d4ba6639e4e26f012b868d5
zk <1597320895@qq.com>
2022-06-11 16:52:16 +0800

    问卷controller 问卷service

--- src/main/java/com/aim/questionnaire/controller/QuestionnaireController.java
+++ src/main/java/com/aim/questionnaire/controller/QuestionnaireController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.aim.questionnaire.service.QuestionnaireService;
 import java.util.HashMap;
 import java.util.Map;
 
